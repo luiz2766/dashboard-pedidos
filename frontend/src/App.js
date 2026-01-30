01d4b68856276658cd597636a291d9acbf2e57bc
luiz2766 <luizhenrique2766@gmail.com>
2026-01-30 14:01:25 -0300

    Atualizando URL da API

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import './App.css';
-
-const API_URL = process.env.REACT_APP_API_URL || 'http://localhost:3001';
+const API_URL = 'https://dashboard.render.com/web/srv-d5ue63kr85hc73agijf0/deploys/dep-d5ue63sr85hc73agijkg';';
 
 function App() {
   const [stats, setStats] = useState(null);
